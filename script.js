d3a7bfe6fb69f9b9c4f5d9367bf6833077eb9506
Ricky Waldt <121530629+rickyharvywaldt@users.noreply.github.com>
2023-08-05 12:00:35 +0700

    Items can now be sorted, but the opacity on the
    dragging class now doesn't work.

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+const sortableList = document.querySelector('.sortable-list');
 const items = document.querySelectorAll('.item');
 
 items.forEach(item => {
     item.addEventListener('dragstart', () => {
-        item.classList.add('dragging');
+        setTimeout(() => item.classList.add('dragging'), 0);
     });
     item.addEventListener('dragend', () => {
         item.classList.remove('dragging');
     });
 });
+
+const initSortableList = (e) => {
+    e.preventDefault();
+    const draggingItem = sortableList.querySelector('.dragging');
+    const siblings = [...sortableList.querySelectorAll('.item:not(.dragging)')];
+
+    let nextSibling = siblings.find(sibling => {
+        return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
+    });
+    sortableList.insertBefore(draggingItem, nextSibling);
+}
+
+sortableList.addEventListener('dragover', initSortableList);
+// sortableList.addEventListener('dragenter', e => e.preventDefault());
 
