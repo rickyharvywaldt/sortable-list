fc8e06fc649e6dc58ffcf5b2c24af2356c3cb78f
Ricky Waldt <121530629+rickyharvywaldt@users.noreply.github.com>
2023-08-05 11:24:25 +0700

    List items can be dragged but not yet sort

---
+++ script.js
@@ -0,0 +1,11 @@
+const items = document.querySelectorAll('.item');
+
+items.forEach(item => {
+    item.addEventListener('dragstart', () => {
+        item.classList.add('dragging');
+    });
+    item.addEventListener('dragend', () => {
+        item.classList.remove('dragging');
+    });
+});
+
